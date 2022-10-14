439e7d8c8f087dd2571b0991ac18ee2d5fcbc310
samuelahmed <samueladuval@gmail.com>
2022-10-14 16:56:20 -0700

    Removed on click close due to closing when clicking on model as well

--- components/PdfModal.js
+++ components/PdfModal.js
@@ -14,7 +14,7 @@ export default function PdfModal() {
         <>
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-            onClick={() => setShowModal(false)}
+            // onClick={() => setShowModal(false)}
           >
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
