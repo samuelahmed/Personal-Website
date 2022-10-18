180e29be7ea9024d91e07834557c037da8f56bec
samuelahmed <samueladuval@gmail.com>
2022-10-17 18:01:12 -0700

    Add resume iframe on modal

--- components/PdfModal.js
+++ components/PdfModal.js
@@ -16,22 +16,10 @@ export default function PdfModal() {
             className="justify-center items-center flex fixed inset-0 z-50 outline-none focus:outline-none"
             onClick={() => setShowModal(false)}
           >
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-5/6 h-5/6 bg-white outline-none focus:outline-none">
-                {/* <div className="relative p-6 flex-auto"> */}
-
-                    <iframe src='resume.pdf' className="h-full aspect-video" />
-                {/* </div> */}
-                {/* <div className="flex items-center justify-end border-t border-solid rounded-b">
-                  <button
-                    className="text-sky-600 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                </div> */}
-              </div>
+            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-5/6 h-5/6 bg-white outline-none focus:outline-none">
+              <iframe src='resume.pdf' className="h-full aspect-video" />
             </div>
+          </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
       ) : null}
