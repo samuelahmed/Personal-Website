3b18e7f0eeed52827a58218af9f4c810a3f03285
samuelahmed <samueladuval@gmail.com>
2022-10-19 16:59:59 -0700

    Add imagines for missing years 93,94,98, future & update logic to go to future if year > 2022

--- components/CustomImage.js
+++ components/CustomImage.js
@@ -2,9 +2,15 @@ import Image from "next/image";
 
 const CustomImage = (props) => {
 
+    let img = props.source
+
+    if (img > 2022) {
+        img = 'future'
+    }
+
     return (
         <div className='relative mx-auto bg-gradient-to-b from-sky-800 dark:from-sky-400 rounded-full w-80 h-80 mt-20 overflow-hidden'>
-            <Image src={`/${props.source}.png`} alt='missing' width='400' height='400' />
+            <Image src={`/${img}.png`} alt='missing' width='400' height='400' />
         </div>
     )
 }
